8f79c562ae5a5d7f799322b063220f92bbf184ab
ktsreddy007 <cloudtech047@gmail.com>
2025-10-01 19:22:38 +0530

    checking file tracker

--- Database/MSSQL_Server/Dev/rollback_handlers/migration_rollback_mapper.sql
+++ Database/MSSQL_Server/Dev/rollback_handlers/migration_rollback_mapper.sql
@@ -1,15 +1,18 @@
+--drop table Payroll.dbo.migration_rollback_mapper;
 IF NOT EXISTS (
      SELECT * FROM INFORMATION_SCHEMA.TABLES
     WHERE TABLE_NAME = 'Payroll.dbo.migration_rollback_mapper' AND TABLE_SCHEMA = 'dbo'
 )
 BEGIN
 CREATE TABLE Payroll.dbo.migration_rollback_mapper (
-    Id INT IDENTITY(1,1) PRIMARY KEY,
-    migration_version VARCHAR(50),               -- Initially empty, to be filled after migration
-    migration_script VARCHAR(1000),               -- Initially empty, to be filled after migration
-    rollback_script_name VARCHAR(1000),           -- Rollback script file name, provided by developer during PR
-    rollback_script_repo_folder VARCHAR(255),    -- Default folder for rollback scripts
-    rollback_script_repo_url VARCHAR(1000),   -- Default URL for rollback scripts
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP  -- Timestamp when the mapping was created
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    migration_version VARCHAR(100),          -- Initially empty, to be filled after migration
+    migration_script VARCHAR(1000),           -- Initially empty, to be filled after migration
+    rollback_script_name VARCHAR(1000),       -- Rollback script file name, provided by developer during PR
+    rollback_script_repo_folder VARCHAR(255),-- Default folder for rollback scripts
+    rollback_script_repo_url VARCHAR(255),   -- Default GitHub repo URL for rollback scripts
+    build_id INT,                            -- Azure DevOps Build ID
+    pr_id INT,                               -- Pull Request ID
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP -- Timestamp when the mapping was created
 );
 END;
