7ebb78112fc9036950de050d5afa11d0de71f237
ktsreddy007 <cloudtech047@gmail.com>
2025-10-03 20:55:39 +0530

    loggges

---
+++ Database/MSSQL_Server/Dev/migrations/V4__createtmp.sql
@@ -0,0 +1,13 @@
+-- Create the 'users' table
+IF NOT EXISTS (
+    SELECT * FROM INFORMATION_SCHEMA.TABLES
+    WHERE TABLE_NAME = 'Payroll.dbo.tempss' AND TABLE_SCHEMA = 'dbo'
+)
+BEGIN
+CREATE TABLE Payroll.dbo.tempss (
+    Id INT IDENTITY(1,1) PRIMARY KEY,
+    Username NVARCHAR(100) NOT NULL,
+    Email NVARCHAR(255) NOT NULL,
+    CreatedAt DATETIME2 DEFAULT GETDATE()
+);
+END
