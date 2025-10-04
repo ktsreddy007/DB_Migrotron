ebe206980efa6eb3f6ba3c096212f96fd0d4c92d
ktsreddy007 <cloudtech047@gmail.com>
2025-10-04 12:25:44 +0530

    just java

---
+++ Database/MSSQL_Server/Dev/rollback_sql/rb__usermama.sql
@@ -0,0 +1,13 @@
+-- Create the 'users' table
+IF NOT EXISTS (
+    SELECT * FROM INFORMATION_SCHEMA.TABLES
+    WHERE TABLE_NAME = 'Payroll.dbo.usermama' AND TABLE_SCHEMA = 'dbo'
+)
+BEGIN
+CREATE TABLE Payroll.dbo.usermama (
+    Id INT IDENTITY(1,1) PRIMARY KEY,
+    Username NVARCHAR(100) NOT NULL,
+    Email NVARCHAR(255) NOT NULL,
+    CreatedAt DATETIME2 DEFAULT GETDATE()
+);
+END
