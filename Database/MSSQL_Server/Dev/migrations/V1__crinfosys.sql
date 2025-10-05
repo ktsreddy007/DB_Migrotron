f79c573fe433636ff92e2c8e89e0cde1b292253f
ktsreddy007 <cloudtech047@gmail.com>
2025-10-05 09:03:33 +0530

    sql snack

--- Database/MSSQL_Server/Dev/migrations/V1__crinfosys.sql
+++ Database/MSSQL_Server/Dev/migrations/V1__crinfosys.sql
@@ -1,9 +1,9 @@
 IF NOT EXISTS (
     SELECT * FROM INFORMATION_SCHEMA.TABLES
-    WHERE TABLE_NAME = 'Payroll.dbo.nations' AND TABLE_SCHEMA = 'dbo'
+    WHERE TABLE_NAME = 'Payroll.dbo.nbinfo' AND TABLE_SCHEMA = 'dbo'
 )
 BEGIN
-CREATE TABLE Payroll.dbo.nations (
+CREATE TABLE Payroll.dbo.nbinfo (
     Id INT IDENTITY(1,1) PRIMARY KEY,
     Username NVARCHAR(100) NOT NULL,
     Email NVARCHAR(255) NOT NULL,
