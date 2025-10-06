acb08dc4dcd67f38a3e6365c15b72b15ead9e95e
ktsreddy007 <cloudtech047@gmail.com>
2025-10-06 21:54:13 +0530

    sanju change

--- Database/MSSQL_Server/Dev/migrations/V1__sanjucreate.sql
+++ Database/MSSQL_Server/Dev/migrations/V1__sanjucreate.sql
@@ -1,10 +1,10 @@
 -- Create the 'users' table
 IF NOT EXISTS (
     SELECT * FROM INFORMATION_SCHEMA.TABLES
-    WHERE TABLE_NAME = 'Payroll.dbo.bar' AND TABLE_SCHEMA = 'dbo'
+    WHERE TABLE_NAME = 'Payroll.dbo.smac' AND TABLE_SCHEMA = 'dbo'
 )
 BEGIN
-CREATE TABLE Payroll.dbo.sar (
+CREATE TABLE Payroll.dbo.smac (
     Id INT IDENTITY(1,1) PRIMARY KEY,
     Username NVARCHAR(100) NOT NULL,
     Email NVARCHAR(255) NOT NULL,
