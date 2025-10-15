befb91e19e051cff555f6c3a936b937e6541d64f
ktsreddy007 <cloudtech047@gmail.com>
2025-10-15 22:19:23 +0530

    efcore added

---
+++ FlywayWithEFCore/sql/V2__AddEmailToUser.sql
@@ -0,0 +1,9 @@
+﻿BEGIN TRANSACTION;
+ALTER TABLE [Users] ADD [Email] nvarchar(max) NOT NULL DEFAULT N'';
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20251015162101_AddEmailToUser', N'9.0.10');
+
+COMMIT;
+GO
+
